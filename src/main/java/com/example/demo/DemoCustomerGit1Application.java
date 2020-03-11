b46dd2d9ff2269dec341d5658bf333d75aa7ea8b
user <user@DESKTOP-2J3O9J2>
2020-03-11 14:59:35 +0530

    commit

---
+++ src/main/java/com/example/demo/DemoCustomerGit1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCustomerGit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCustomerGit1Application.class, args);
+	}
+
+}
